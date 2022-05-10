03ab49b77ab4d0c5d2a90f7dc02b835fee4c3ae6
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-05-10 17:11:23 -0500

    Update setup-lvm.sh

--- setup-lvm.sh
+++ setup-lvm.sh
@@ -1,7 +1,7 @@
 LVM_DISK=sda
 LVM_PART=sda4
 
-apk add cfdisk lvm2
+apk add cfdisk lvm2 lvm2-extra e2fsprogs e2fsprogs-extra
 cfdisk /dev/${LVM_DISK}
 pvcreate /dev/${LVM_PART}
 vgcreate vg0 /dev/${LVM_PART}
