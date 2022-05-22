0d1ac42b943b859dff2e9583fbc329ae38b87628
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-05-22 10:18:02 -0500

    Update setup-openldap.sh

--- setup-openldap.sh
+++ setup-openldap.sh
@@ -1,9 +1,9 @@
 export DOMAIN="dc=home"
 
-echo "Installing packages..."
+echo "Installing packages"
 apk add openldap openldap-back-mdb openldap-clients
 
-echo "Configuring for v2.3+ style slapd.d config directory..."
+echo "Configuring for v2.3+ style slapd.d config directory"
 install -m 755 -o ldap -g ldap -d /etc/openldap/slapd.d
 sed -i~ \
   -e 's/^cfgfile=/#cfgfile=/' \
@@ -11,20 +11,20 @@ sed -i~ \
   /etc/conf.d/slapd
 rm /etc/openldap/slapd.conf
 
-echo "Customizing for domain: ${DOMAIN}..."
+echo "Configuring for domain: ${DOMAIN}"
 sed -i~ \
   -e 's/\.la$/.so/' \
   -e "s/dc=my-domain,dc=com/${DOMAIN}/" /etc/openldap/slapd.ldif
 
-echo "Adding schema for Linux user accounts..."
+echo "Adding schema for Linux user accounts"
 cp /etc/openldap/slapd.ldif /etc/openldap/slapd.ldif~1
 awk '{ print } /^include:/ { sub("core", "cosine", $0); print $0; sub("cosine", "inetorgperson"); print $0; sub("inetorgperson", "nis"); print  }' /etc/openldap/slapd.ldif~1 >/etc/openldap/slapd.ldif
 
-echo "Importing configuration..."
+echo "Importing configuration"
 slapadd -n 0 -F /etc/openldap/slapd.d -l /etc/openldap/slapd.ldif
 chown -R ldap:ldap /etc/openldap/slapd.d/*
 
-echo "Configuring slapd service..."
+echo "Starting slapd service"
 install -m 755 -o ldap -g ldap -d /var/lib/openldap/run
 service slapd start
 rc-update add slapd
