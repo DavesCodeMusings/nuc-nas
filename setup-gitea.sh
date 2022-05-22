0babd71d1be972a8b6df3aa6ccdf31c06e277920
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-05-22 10:06:39 -0500

    Update setup-gitea.sh

--- setup-gitea.sh
+++ setup-gitea.sh
@@ -1,14 +1,20 @@
 GIT_HOME=/srv/git
+COMPOSE_PROJECT_DIR=/var/lib/docker/compose/gitea
 
+echo "Creating compose project directory"
+mkdir -p ${COMPOSE_PROJECT_DIR} || exit 1
+
+echo "Creating git user and group"
 grep ^git /etc/passwd || adduser -D -h ${GIT_HOME} git
 GIT_UID=$(id -u git)
 GIT_GID=$(id -g git)
 [ -d ${GIT_HOME} ] || mkdir ${GIT_HOME}
 
+echo "Installing packages"
 apk add git
 
-mkdir /var/lib/docker/compose/gitea
-cat <<EOF >/var/lib/docker/compose/gitea/docker-compose.yml
+echo "Creating compose file"
+cat <<EOF >${COMPOSE_PROJECT_DIR}/compose.yml
 services:
     gitea:
         image: gitea/gitea:latest
@@ -31,7 +37,8 @@ volumes:
     data:
 EOF
 
-cd /var/lib/docker/compose/gitea
+echo "Starting Gitea"
+cd ${COMPOSE_PROJECT_DIR}
 docker-compose up -d
 
 echo "Visit http://$(hostname):3000 to configure Gitea"
