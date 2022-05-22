af55cc8824d703d2f1cb2cbc9ee5f735ecca89e1
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-05-22 10:28:53 -0500

    Update setup-pihole.sh

--- setup-pihole.sh
+++ setup-pihole.sh
@@ -1,9 +1,13 @@
-mkdir /var/lib/docker/compose/pihole || exit 1
+PIHOLE_ADMIN_PORT=8080
+PIHOLE_ADMIN_PASSWORD=password
+COMPOSE_PROJECT_DIR=/var/lib/docker/compose/pihole
+
+echo "Creating compose project directory"
+mkdir -p ${COMPOSE_PROJECT_DIR} || exit 1
 cd /var/lib/docker/compose/pihole || exit 2
 
-cat <<EOF >docker-compose.yml
-version: "3"
-
+echo "Creating compose file"
+cat <<EOF >${COMPOSE_PROJECT_DIR}/compose.yml
 # More info at https://github.com/pi-hole/docker-pi-hole/ and https://docs.pi-hole.net/
 services:
   pihole:
@@ -15,10 +19,10 @@ services:
       - "53:53/tcp"
       - "53:53/udp"
 #      - "67:67/udp" # Only required if you are using Pi-hole as your DHCP server
-      - "80:80/tcp"
+      - "${PIHOLE_ADMIN_PORT}:80/tcp"
     environment:
       TZ: 'America/Chicago'
-      WEBPASSWORD: password
+      WEBPASSWORD: ${PIHOLE_ADMIN_PASSWORD}
     # Volumes store your data between container upgrades
     volumes:
       - 'pihole:/etc/pihole'
@@ -33,4 +37,6 @@ volumes:
     dnsmasq.d:
 EOF
 
+echo "Starting Gitea"
+cd ${COMPOSE_PROJECT_DIR}
 docker-compose up -d
