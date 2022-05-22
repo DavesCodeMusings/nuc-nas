10bec81b25132361e6a6bde80750338bc5e77ef5
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-05-22 10:12:30 -0500

    Update setup-lvm.sh

--- setup-lvm.sh
+++ setup-lvm.sh
@@ -1,9 +1,16 @@
 LVM_DISK=sda
 LVM_PART=sda4
 
+echo "Installing packages"
 apk add cfdisk lvm2 lvm2-extra e2fsprogs e2fsprogs-extra
+
+echo "Creating LVM partition"
 cfdisk /dev/${LVM_DISK}
 pvcreate /dev/${LVM_PART}
+
+echo "Creating volume group vg0"
 vgcreate vg0 /dev/${LVM_PART}
 vgchange -ay
+
+echo "Configuring LVM to start at boot"
 rc-update add lvm boot
