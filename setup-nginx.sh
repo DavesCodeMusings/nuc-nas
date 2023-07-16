7dbbe08d91e050c3946785293c2893fae379fe71
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2023-07-15 22:14:53 -0500

    Create setup-nginx.sh

---
+++ setup-nginx.sh
@@ -0,0 +1,75 @@
+SSL_CERT=/etc/ssl/certs/$(hosname).crt
+SSL_KEY=/etc/ssl/private/$(hostnam).key
+
+if ! [ -f SSL_CERT && -f SSL_KEY ]; then
+  echo "Certificates not found!"
+  echo "Press CTRL+C to abort or ENTER to ignore."
+  read REPLY
+fi
+
+echo "Creating compose project directory"
+mkdir -p /var/lib/docker/compose/nginx || exit 2
+
+echo "Creating Nginx compose file"
+if [ -f SSL_CERT && -f SSL_KEY ]; then
+  cat <<EOF >/var/lib/docker/compose/nginx/compose.yml
+services:
+    nginx:
+        image: nginx
+        container_name: nginx
+        hostname: nginx
+        restart: unless-stopped
+        ports:
+        - 80:80
+        - 443:443
+        volumes:
+        - /etc/ssl:/etc/ssl:ro
+        - ./conf.d:/etc/nginx/conf.d
+        - /srv/www:/usr/share/nginx/html:ro
+EOF
+else
+  cat <<EOF >/var/lib/docker/compose/nginx/compose.yml
+services:
+    nginx:
+        image: nginx
+        container_name: nginx
+        hostname: nginx
+        restart: unless-stopped
+        ports:
+        - 80:80
+        volumes:
+        - ./conf.d:/etc/nginx/conf.d
+        - /srv/www:/usr/share/nginx/html:ro
+EOF
+fi
+
+echo "Creating static web server configuration"
+mkdir /var/lib/docker/compose/nginx/conf.d || exit 3
+if [ -f SSL_CERT && -f SSL_KEY ]; then
+  cat <<EOF >/var/lib/docker/compose/nginx/conf.d/00_default.conf
+# Serve static files
+server {
+    listen 80;
+    listen 443 ssl;
+    server_name  anubis.home;
+    ssl_certificate  $SSL_CERT;
+    ssl_certificate_key  $SSL_KEY;
+    location / {
+        root /usr/share/nginx/html/anubis-www;
+        index index.html index.htm;
+    }
+}
+EOF
+else
+  cat <<EOF >/var/lib/docker/compose/nginx/conf.d/00_default.conf
+# Serve static files
+server {
+    listen 80;
+    server_name  anubis.home;
+    location / {
+        root /usr/share/nginx/html/anubis-www;
+        index index.html index.htm;
+    }
+}
+EOF
+fi
