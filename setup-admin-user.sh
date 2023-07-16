1192aec1515d21d86dca169fd0110c3752e85216
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2023-07-16 13:05:24 -0500

    Create setup-admin-user.sh

---
+++ setup-admin-user.sh
@@ -0,0 +1,15 @@
+USERNAME=admin
+FULLNAME="System Administrator"
+
+echo "Installing packages"
+apk update
+apk add sudo
+
+echo "Creating user account: $USERNAME"
+adduser -g '$FULLNAME' $USERNAME
+addgroup $USERNAME wheel
+
+echo "Configuring sudo"
+cat <<EOF >/etc/sudoers.d/nopasswd
+%wheel ALL=(ALL) NOPASSWD: ALL
+EOF
