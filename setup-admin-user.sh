ae8a9a5ff7d95f2f79962ad7724e3c39d66d5065
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2023-07-16 13:25:03 -0500

    Update setup-admin-user.sh

--- setup-admin-user.sh
+++ setup-admin-user.sh
@@ -5,14 +5,12 @@ echo "Installing packages"
 apk update
 apk add sudo
 
-echo "Creating user account: $USERNAME"
-adduser -g '$FULLNAME' $USERNAME
-addgroup $USERNAME wheel
-
 echo "Configuring sudo"
 cat <<EOF >/etc/sudoers.d/nopasswd
 %wheel ALL=(ALL) NOPASSWD: ALL
 EOF
 
-echo "Setting password for $USERNAME"
+echo "Creating user account: $USERNAME"
+adduser -g '$FULLNAME' $USERNAME
+addgroup $USERNAME wheel
 passwd $USERNAME
