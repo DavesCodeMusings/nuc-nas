a040368bbb0ad65a0dba16a5137ce48535790626
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2023-07-16 22:08:41 -0500

    Update setup-cert-authority.sh

--- setup-cert-authority.sh
+++ setup-cert-authority.sh
@@ -46,6 +46,35 @@ openssl genrsa -out ${KEYS_DIR}/${ROOT_CA}.key 4096
 openssl req -config ${TEMP_DIR}/${ROOT_CA}.conf -key ${KEYS_DIR}/${ROOT_CA}.key -new -out ${TEMP_DIR}/${ROOT_CA}.csr
 openssl x509 -trustout -signkey ${KEYS_DIR}/${ROOT_CA}.key -days 3650 -req -in ${TEMP_DIR}/${ROOT_CA}.csr -out ${CERTS_DIR}/${ROOT_CA}.crt
 
+echo "Creating intermediate CA config"
+cat <<EOF >${TEMP_DIR}/home.conf
+[req]
+default_bits = 2048
+default_md = sha256
+distinguished_name = req_dn
+req_extensions = req_ext
+prompt = no
+
+[req_dn]
+C = ${COUNTRY}
+ST = ${STATE_PROVINCE}
+L = ${CITY}
+O = ${DOMAIN}
+CN = ${DOMAIN}
+
+[req_ext]
+basicConstraints = critical, CA:true
+keyUsage = keyCertSign, cRLSign
+EOF
+
+echo "Creating intermediate CA"
+openssl genrsa -out ${KEYS_DIR}/${DOMAIN}.key 2048
+openssl req -config ${TEMP_DIR}/${DOMAIN}.conf -key ${KEYS_DIR}/${DOMAIN}.key -new -out ${TEMP_DIR}/${DOMAIN}.csr
+openssl x509 -trustout -req -CA ${CERTS_DIR}/${ROOT_CA}.crt -CAkey ${KEYS_DIR}/${ROOT_CA}.key -in ${TEMP_DIR}/${DOMAIN}.csr -out ${CERTS_DIR}/${DOMAIN}.crt -days 3650
+
 echo "Cleaning up temporary files"
 rm ${TEMP_DIR}/${ROOT_CA}.csr
 rm ${TEMP_DIR}/${ROOT_CA}.conf
+rm ${TEMP_DIR}/${DOMAIN}.csr
+rm ${TEMP_DIR}/${DOMAIN}.conf
+rmdir ${TEMP_DIR}
